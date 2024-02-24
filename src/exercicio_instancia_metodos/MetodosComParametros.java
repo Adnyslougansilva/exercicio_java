dd88037b50e0c1e9a2668fd963eeb5689985e33c
Adnyslougansilva <adnyslougan@gmail.com>
2024-02-24 11:34:42 -0300

    novos exercicios

---
+++ src/exercicio_instancia_metodos/MetodosComParametros.java
@@ -0,0 +1,20 @@
+////package exercicio_instancia_metodos;
+//
+//import javax.lang.model.util.SimpleTypeVisitor14;
+//
+//public class MetodosComParametros {
+//
+//    public static void metodoUmSemRetorno(String nome){
+//        System.out.println(nome);
+//    }
+//
+//    public static String metodoUmComRetorno(String nome) {
+//        return "O parametro enviado é: "+nome;
+//    }
+//
+//    public static void main(String[] args) {
+//       metodoUmSemRetorno("Adnys");
+//       metodoUmComRetorno();
+//    }
+//
+//}
