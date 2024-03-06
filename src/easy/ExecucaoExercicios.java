84865027fbcdd7f4a94dc44621850cbe7e5993f3
Adnyslougansilva <adnyslougan@gmail.com>
2024-03-05 21:34:55 -0300

    Refatoração Ex3 easy

--- src/easy/ExecucaoExercicios.java
+++ src/easy/ExecucaoExercicios.java
@@ -1,8 +1,17 @@
 package easy;
 
+import javax.swing.*;
+
 public class ExecucaoExercicios {
+
     public static void main(String[] args) {
-        Exercicio02_easy exercicio02Easy = new Exercicio02_easy();
-        System.out.println(exercicio02Easy.exibirPalavra("Estudo faz bem!"));
-    }
+//        Exercicio02_easy exercicio02Easy = new Exercicio02_easy();
+//        System.out.println(exercicio02Easy.exibirPalavra("Estudo faz bem!"));
+
+        Exercicio03_easy exercicio03_easy = new Exercicio03_easy();
+        String primeiroValor = JOptionPane.showInputDialog("Digite um valor (pode ser numerico ou texto)!");
+        String segundoValor = JOptionPane.showInputDialog("Digite um valor (pode ser numerico ou texto!");
+        String texto = exercicio03_easy.trocaValorDigitado(primeiroValor, segundoValor);
+        System.out.println(texto);
+  }
 }
--- src/easy/Exercicio03_easy.java
+++ src/easy/Exercicio03_easy.java
@@ -4,11 +4,7 @@ import javax.swing.*;
 
 public class Exercicio03_easy {
 
-    public static void main(String[] args) {
-        String primeiroValor = JOptionPane.showInputDialog("Digite um valor (pode ser numerico ou texto)!");
-        String segundoValor = JOptionPane.showInputDialog("Digite um valor (pode ser numerico ou texto!");
-
-        System.out.println("Primeiro valor = " + segundoValor);
-        System.out.println("Segundo valor = " + primeiroValor);
+    public String trocaValorDigitado(String primeiroValor, String segundoValor) {
+       return "Primeiro valor: " + segundoValor + "|Segundo valor: " + primeiroValor;
     }
 }
