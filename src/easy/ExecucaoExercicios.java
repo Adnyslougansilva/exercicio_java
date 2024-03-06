69c956dfbd879ae9ea96c0a51a0c48caea7f5ee3
Adnyslougansilva <adnyslougan@gmail.com>
2024-03-05 22:11:11 -0300

    Refatoração Ex5 easy

--- src/easy/ExecucaoExercicios.java
+++ src/easy/ExecucaoExercicios.java
@@ -14,8 +14,18 @@ public class ExecucaoExercicios {
 //        String texto = exercicio03_easy.trocaValorDigitado(primeiroValor, segundoValor);
 //        System.out.println(texto);
 
-        Exercicio04_easy exercicio04_easy = new Exercicio04_easy();
-        int numeroDigitado = Integer.valueOf(JOptionPane.showInputDialog("Digite um numero inteiro!"));
-        System.out.println("O Dobro do numero é  = " + exercicio04_easy.calcularDobro(numeroDigitado));
+//        Exercicio04_easy exercicio04_easy = new Exercicio04_easy();
+//        int numeroDigitado = Integer.valueOf(JOptionPane.showInputDialog("Digite um numero inteiro!"));
+//        System.out.println("O Dobro do numero é  = " + exercicio04_easy.calcularDobro(numeroDigitado));
+
+        int num1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o Primeiro Número Inteiro!"));
+        int num2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o Segundo Número Inteiro!"));
+        int num3 = Integer.parseInt(JOptionPane.showInputDialog("Digite o Terceiro Número Inteiro!"));
+
+        Exercicio05_easy exercicio05_easy = new Exercicio05_easy();
+        System.out.println("A soma dos números é: " + exercicio05_easy.calcularSoma(num1, num2, num3));
+        System.out.println("A subtração dos números é: " + exercicio05_easy.calcularSubtrair(num1, num2, num3));
+        System.out.println("A multiplicação dos números é: " + exercicio05_easy.calcularMultiplicar(num1, num2, num3));
+        System.out.println("A Média dos números é: " + exercicio05_easy.calcularMedia(num1, num2, num3));
   }
 }
